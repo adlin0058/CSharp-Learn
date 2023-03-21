16960390aacf797ab928480529f0d28bb430920a
MiKu-Ming <2412780034@qq.com>
2023-03-21 14:42:06 +0800

    属性，匿名数据类型

---
+++ 11-属性/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _11_属性
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            customer lm = new customer();
+
+            lm.Age = 21;
+            lm.Name = "liming";
+            lm.Address = "shanghai";
+            lm.creatTime = "2023年";
+            Console.WriteLine(lm.Age);
+            Console.WriteLine(lm.Name);
+            Console.WriteLine(lm.Address);
+            Console.WriteLine(lm.creatTime);
+            lm.Show();
+
+            //匿名数据类型
+            var age2 = 22;
+            //给age2初始化后，类型确定，不可修改类型
+            //可赋值，相同类型
+            age2 = 33;
+            age2 = 44;
+            Console.WriteLine(age2);
+
+        }
+    }
+}
---
+++ 11-属性/customer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_属性
+{
+    internal class customer
+    {
+        public string name;
+        public string address;
+        private int age;
+        public string creatTime;
+
+        //只有一个get为只读
+        //只有一个set为只写
+        public int Age
+        {
+            get 
+            { 
+                return age; 
+            }
+            set
+            {
+                if (value < 0) return;
+                age = value; 
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value; 
+            }
+        }
+        public string Address
+        {
+            get 
+            {
+                return address;
+            }
+            set
+            {
+                address = value;
+            }
+        }
+
+        public string CreatTime
+        {
+            get
+            {
+                return creatTime;
+            }
+            set
+            {
+                creatTime = value;
+            }
+        }
+        public void Show()
+        {
+            Console.WriteLine("名字：" + name);
+            Console.WriteLine("年龄：" + age);
+            Console.WriteLine("地址：" + address);
+            Console.WriteLine("创建时间：" + creatTime);
+        }
+    }
+}
