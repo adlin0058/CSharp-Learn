98c8ef4275b95f6ab836bcfb627cbdc16a17b54d
MiKu-Ming <2412780034@qq.com>
2023-03-24 16:52:50 +0800

    自己的集合类

---
+++ 18-自己的集合类/MyList.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_自己的集合类
+{
+    internal class MyList<T>
+    {
+        private T[] list = new T[0];
+
+        //显示
+        public void Show()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(list[i] + " ");
+            }
+        }
+
+        //Capacity获取容量大小
+        public int Capacity//属性
+        {
+            get { return list.Length; }
+        }
+
+        //Count获取元素个数
+        private int count = 0;
+
+        public int Count
+        { get { return count; } }
+
+        //索引器
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index > count - 1)
+                    throw new IndexOutOfRangeException("索引超出范围");
+                else
+                    return list[index];
+            }
+            set
+            {
+                if (index < 0 || index > count - 1)
+                    throw new IndexOutOfRangeException("索引超出范围");
+                list[index] = value;
+            }
+        }
+
+        //添加元素
+        public void Add(T item)
+        {
+            if (Capacity == 0)
+            {
+                list = new T[4];
+            }
+            if (count >= Capacity)
+            {
+                T[] temp = new T[Capacity * 2];
+                for (int i = 0; i < list.Length; i++)
+                {
+                    temp[i] = list[i];
+                }
+                list = temp;
+            }
+            list[count] = item;
+            count++;
+        }
+
+        //插入
+        public void Insert(int index, T item)
+        {
+            if (index > count || index < 0)
+            {
+                Console.WriteLine(-1);
+            }
+            else
+            {
+                if (Capacity == 0)
+                {
+                    list = new T[4];
+                    list[index] = item;
+                    count++;
+                }
+                if (count >= Capacity)
+                {
+                    T[] temp = new T[Capacity * 2];
+                    for (int i = 0; i < list.Length; i++)
+                    {
+                        temp[i] = list[i];
+                    }
+                    list = temp;
+                }
+                for (int i = count; i > index; i--)
+                {
+                    list[i] = list[i - 1];
+                }
+                list[index] = item;
+                count++;
+            }
+        }
+
+        //删除
+        public void RemoveAt(int index)
+        {
+            if (index > count - 1 || index < 0)
+            {
+                Console.WriteLine(-1);
+            }
+            else
+            {
+                if (Capacity == 0)
+                {
+                    Console.WriteLine(-1);
+                }
+                else
+                {
+                    for (int i = index; i < count - 1; i++)
+                    {
+                        list[i] = list[i + 1];
+                    }
+                    count--;
+                }
+            }
+        }
+
+        //查找从前往后
+        public int IndexOf(T item)
+        {
+            int index = -1;
+            for (int i = 0; i < count; i++)
+            {
+                if (item.Equals(list[i]))
+                {
+                    index = i; break;
+                }
+            }
+            return index;
+        }
+
+        //从后往前
+        public int LastIndexOf(T item)
+        {
+            int index = -1;
+            for (int i = count - 1; i >= 0; i--)
+            {
+                if (item.Equals(list[i]))
+                {
+                    index = i; break;
+                }
+            }
+            return index;
+        }
+
+        //排序从小到大
+        public void Sort()
+        {
+            Array.Sort(list, 0, count);
+        }
+    }
+}
---
+++ 18-自己的集合类/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace _18_自己的集合类
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //List<int> list = new List<int>() { 1, 2, 3, 4 };
+            //list.Add(1);
+            //Console.WriteLine(list.Capacity);
+            //list.Insert(0, 2);
+            //Console.WriteLine(list[1]);
+            //Console.WriteLine(list.Count);
+            //list.RemoveAt(0);
+            //Console.WriteLine(list.IndexOf(2));
+            //Console.WriteLine(list.LastIndexOf(2));
+            //list.Sort();
+
+            MyList<int> mylist = new MyList<int>();
+            Console.WriteLine(mylist.Capacity);
+            mylist.Add(0);
+            mylist.Add(1);
+            mylist.Add(2);
+            mylist.Add(3);
+            mylist.Add(4);
+            mylist.Add(5);
+            mylist.Add(7);
+            mylist.Add(8);
+            mylist.Add(9);
+            mylist.Insert(6, 2);
+            mylist.Insert(3, 88);
+
+            Console.WriteLine("capacity = " + mylist.Capacity);
+            Console.WriteLine("count = " + mylist.Count);
+
+            for (int i = 0; i < mylist.Count; i++)
+            {
+                Console.Write(mylist[i] + " ");
+            }
+            Console.WriteLine();
+            mylist.RemoveAt(11);
+            mylist.Insert(17, -1);
+            for (int i = 0; i < mylist.Count; i++)
+            {
+                Console.Write(mylist[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("从前到后" + mylist.IndexOf(2));
+            Console.WriteLine("从后到前" + mylist.LastIndexOf(2));
+            mylist.Sort();
+            mylist.Show();
+        }
+    }
+}
