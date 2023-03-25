5453fda24aaf5bb845c46b0a622adbf7a82b201e
MiKu-Ming <2412780034@qq.com>
2023-03-25 17:39:38 +0800

    Action&Func委托

---
+++ 21-Action委托/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _21_Action委托
+{
+    internal class Program
+    {
+        private static void Test1()
+        {
+            Console.WriteLine("Test1");
+        }
+
+        private static void Test2(int x)
+        {
+            Console.WriteLine("Test2" + x);
+        }
+
+        private static void Test3(int x, double y)
+        {
+            Console.WriteLine("Test3" + x + y);
+        }
+
+        private static void Main(string[] args)
+        {
+            //Action 是泛型，可有任意参数，但是一定无返回值
+            Action method = Test1;
+            method();
+
+            Action<int> method2 = Test2;
+            method2(666);
+
+            Action<int, double> method3 = Test3;
+            method3(2233, 88.88);
+        }
+    }
+}
---
+++ 22-Fuc委托/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _22_Func委托
+{
+    internal class Program
+    {
+        private static string Test1()
+        {
+            return "Test1";
+        }
+
+        private static string Test2(int x, double y)
+        {
+            return "Test2" + x + y;
+        }
+
+        private static void Main(string[] args)
+        {
+            //Func 参数任意    必须有返回值
+            //调用时↓  必须说明返回值类型
+            Func<string> func1 = Test1;
+            Console.WriteLine(func1());
+
+            //若有参数，↓应在返回值类型前写明所有参数类型
+            Func<int, double, string> func2 = Test2;
+            Console.WriteLine(func2(22, 22.33));
+        }
+    }
+}
