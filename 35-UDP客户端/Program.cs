32bb9f05e05c7449e13860126b290991eedc81a5
MiKu-Ming <2412780034@qq.com>
2023-04-04 21:51:33 +0800

    UDP

---
+++ 35-UDP客户端/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace _35_UDP客户端
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Socket udpClient = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            //创建ip和端口
+            //IP                                     本机ip地址↓
+            IPAddress iP = new IPAddress(new byte[] { 10, 200, 4, 39 });
+            //IP + port（端口）
+            IPEndPoint iPEndPoint = new IPEndPoint(iP, 2233);
+
+            byte[] message = Encoding.UTF8.GetBytes("哈哈哈，客户端来喽~");
+
+            udpClient.SendTo(message, iPEndPoint);
+        }
+    }
+}
---
+++ 35-UDP服务端/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace _35_UDP服务端
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Socket udpServer = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            //创建ip和端口
+            //IP                                     本机ip地址↓
+            IPAddress iP = new IPAddress(new byte[] { 10, 200, 4, 39 });
+            //IP + port（端口）
+            IPEndPoint iPEndPoint = new IPEndPoint(iP, 2233);
+
+            //绑定端口
+            udpServer.Bind(iPEndPoint);
+
+            //可接收 来自任意地址、任意端口的信息
+            IPEndPoint ip = new IPEndPoint(IPAddress.Any, 0);
+            byte[] buffer = new byte[1024];
+
+            EndPoint endPoint = (EndPoint)ip;
+            //                          ↓执行到此处等待接收
+            int length = udpServer.ReceiveFrom(buffer, ref endPoint);
+            string message = Encoding.UTF8.GetString(buffer, 0, length);
+            Console.WriteLine("接收到：" + message);
+        }
+    }
+}
